365d54e406947bd9ce45125efcb690ff313e242f
@ki <appsilver55@gmail.com>
2022-01-16 17:32:35 +0900

    feat:constファイルの修正

--- CONST.js
+++ CONST.js
@@ -1,2 +1,8 @@
-const URL_MAIN =  'C:\Users\surfa\OneDrive\デスクトップ\maching\main.html',
+export default URL_MAIN =  'C:\Project\main.html';
+export default URL_STYLE =  'C:\Project\style\style.css';
+export default URL_IMG_ID =  'C:\Project\img\lineID.jpg';
+export default URL_IMG_FR=  'C:\Project\img\freedeai.jpg';
 
+//export default k = 12;
+
+// module.exports = URL_MAIN;
